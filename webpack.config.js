773ed499b52508f0556de54b0e0b002f33f39362
u-master <to-umaster@mail.ru>
2020-05-28 16:38:14 +0600

    add html plugin at webpack, index page template

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -7,4 +8,10 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'RSS Reader',
+      template: './src/index.html',
+    }),
+  ],
 };
